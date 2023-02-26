943b9201c1ef8484eb2ab0d906831044dd1f257d
micerr <fbmichele@hotmail.it>
2023-02-26 14:20:04 +0100

    added code for tracking and metrics estimator

--- src/preproc.py
+++ src/preproc.py
@@ -4,6 +4,9 @@ import os
 import elikopy
 import elikopy.utils
 
+preproc_steps = [None, "None", "denoising", "gibbs", "topup", "eddy", "biasfield", "report", "topup_synb0DisCo_Registration", "topup_synb0DisCo_Inference", "topup_synb0DisCo_Apply", "topup_synb0DisCo_topup"]
+models_steps = [None, "dti", "noddi", "diamond", "mf"]
+
 def main():
     ## Defining Parameters
     onSlurm = False
@@ -50,7 +53,7 @@ def main():
     if "-s" in sys.argv[1:]:
         parIdx = sys.argv.index["-s"] + 1 # the index of the parameter after the option
         par = sys.argv[parIdx]
-        assert par in (None, "None", "denoising", "gibbs", "topup", "eddy", "biasfield", "report", "topup_synb0DisCo_Registration", "topup_synb0DisCo_Inference", "topup_synb0DisCo_Apply", "topup_synb0DisCo_topup"), 'invalid starting state!'
+        assert par in preproc_steps or par in models_steps or par in ("white_mask", "tracking"), 'invalid starting state!'
         starting_state = par
 
     ## Init
@@ -58,50 +61,98 @@ def main():
     study.patient_list() #TODO Check if our volumes are acquired in reverse phase encoding
 
     ## Preprocessing
-    ## RESLICING & BET
-    study.preproc(
-        # Starting STATE
-        starting_state=starting_state,
+    if starting_state in preproc_steps:
+        study.preproc(
+            # Starting STATE
+            starting_state=starting_state,
 
-        # Reslicing
-        reslice=True, #TODO chiedere se le nostre immagini sono interpolate, perche se non lo sono questo può essere anche a False
-        reslice_addSlice=False, # Because we don't perform slice-to-volume corrections
+            # Reslicing
+            reslice=False, #TODO chiedere se le nostre immagini sono interpolate, perche se non lo sono questo può essere anche a False
+            reslice_addSlice=False, # Because we don't perform slice-to-volume corrections
+
+            # Brain Extraction
+            #bet_median_radius=,
+            #bet_numpass=,
+            #bet_dilate=,
+
+            ## MPPCA denoising
+            denoising=True,
+
+            ## GIBBS Ringing Correction
+            gibbs=False,
+
+            ## Susceptibility field estimation
+            topup=True,
+            topupConfig=None, #TODO What is it?
+            forceSynb0DisCo=True,
+
+            ## EDDY and MOTION correction
+            #TODO Studiare come si generano gli artefatti
+            #TODO Capire cosa fa FSL per correggere eddy currents e outlier replacement
+            eddy=True,
+            #niter=,
+            #s2v=, # Doing only the Volume-to-Volume preproc is good enough
+            cuda=cuda,
+            cuda_name="eddy_cuda10.2", # Depends on the version that you have installed
+            #olrep=,
+
+            # Registration
+            qc_reg=False,
+
+            ## BIAS FIELD correction
+            biasfield=True,
+            #biasfield_bsplineFitting=,
+            #biasfield_convergence=,
+        )
+        starting_state = None # In this way it enter in the next steps.
+
+    """
+    Providing a white matter mask is a useful step to accelerate 
+    microstructural features computation and more easily do tractography.
+    """
+    if starting_state in ("white_mask", None):
+        study.white_mask(
+            maskType="wm_mask_FSL_T1"
+        )
+        starting_state=None
+
+    if starting_state in ("tracking", None):
+        study.tracking()
+        starting_state=None
     
-        # Brain Extraction
-        #bet_median_radius=,
-        #bet_numpass=,
-        #bet_dilate=,
-
-        ## MPPCA denoising
-        denoising=True,
-
-        ## GIBBS Ringing Correction
-        gibbs=False,
-
-        ## Susceptibility field estimation
-        topup=True,
-        topupConfig=None, #TODO What is it?
-        forceSynb0DisCo=True,
-
-        ## EDDY and MOTION correction
-        #TODO Studiare come si generano gli artefatti
-        #TODO Capire cosa fa FSL per correggere eddy currents e outlier replacement
-        eddy=True,
-        #niter=,
-        #s2v=, # Doing only the Volume-to-Volume preproc is good enough
-        cuda=cuda,
-        cuda_name="eddy_cuda10.2", # Depends on the version that you have installed
-        #olrep=,
-
-        # Registration
-        qc_reg=False,
-
-        ## BIAS FIELD correction
-        biasfield=True,
-        #biasfield_bsplineFitting=,
-        #biasfield_convergence=,
-    )
-
+    ## Metrics Estimation 
+    
+    if starting_state in models_steps:
+        mask_to_use = "brain_mask_dilated"
+        if starting_state in ("dti", None):
+            study.dti(
+                # mask to use
+                maskType=mask_to_use,
+                # if you want to other mask you have to compute it singularly with white_mask() 
+            )
+            starting_state = None
+        if starting_state in ("diamond", None):
+            study.diamond(
+                # mask to use
+                maskType=mask_to_use,
+                # other params
+                customDiamond="", #TODO add or change it
+            )
+            starting_state = None
+        if starting_state in ("noddi", None):
+            study.noddy(
+                # mask to use
+                maskType=mask_to_use,
+                #lambda_iso_diff=,
+                #lambda_par_diff=,
+            )
+            starting_state = None
+        # if starting_state in ("mf", None):
+        #     study.fingerprinting(
+        #         #dictionary_path=,
+        #         maskType=mask_to_use,
+        #     )
+      
     return 0
 
 
