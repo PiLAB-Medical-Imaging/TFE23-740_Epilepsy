69adb9ae83549cb1fe05e9fde66c4867f4ea4655
micerr <fbmichele@hotmail.it>
2023-04-18 22:48:37 +0200

    .

---
+++ src/seg_job.py
@@ -0,0 +1,15 @@
+# THIS MINI PROGRAM RUN THE SEGMENTATION FOR ALL THE PATIENTS
+import subprocess
+import os
+absolute_path = os.path.dirname(__file__) # return the abs path of the folder of this file, wherever it is
+project_dir = absolute_path + "/.."
+
+nPatients = 23
+
+# srun recon-all -all -s $SUB_ID -i $PROJECT_DIR/study/T1/${SUB_ID}_T1.nii.gz -T2 $PROJECT_DIR/study/T1/${SUB_ID}_T2.nii.gz -T2pial -qcache
+# srun --cpus-per-task=4 mri_cc -force -f -aseg aseg.mgz -o aseg.auto_CCseg.mgz $SUB_ID
+
+for i in range(1,nPatients+1):
+    bashCommand = "srun recon-all -all -s VNSLC_%02d -i %s/study/T1/VNSLC_%02d_T1.nii.gz -T2 %s/study/T1/VNSLC_%02d_T2.nii.gz -T2pial -qcache" % (i, project_dir, i, project_dir, i)
+    #process = subprocess.Popen(bashCommand, universal_newlines=True, shell=True)
+    print(bashCommand)
--- src/seg_job.sh
+++ src/seg_job.sh
@@ -6,8 +6,7 @@
 #SBATCH --output=seg_out.txt
 #SBATCH --error=seg_err.txt
 #
-#SBATCH --ntasks=4
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=23
 #SBATCH --time=0
 #SBATCH --time-min=10:0:0
 #SBATCH --mem-per-cpu=4000
@@ -17,8 +16,7 @@ module load freesurfer
 PROJECT_DIR=$HOME/Epilepsy-dMRI-VNS # It works only in a working directory in which the project is in Home
 export SUBJECTS_DIR=$PROJECT_DIR/seg_subjs
 
-SUB_ID=$1
+srun python3 seg_job.py > out.txt 2>&1 &
+jobs
 
-srun recon-all -all -s $SUB_ID -i $PROJECT_DIR/study/T1/${SUB_ID}_T1.nii.gz -T2 $PROJECT_DIR/study/T1/${SUB_ID}_T2.nii.gz -T2pial -qcache
-
-# srun --cpus-per-task=4 mri_cc -force -f -aseg aseg.mgz -o aseg.auto_CCseg.mgz $SUB_ID
+exit 0
