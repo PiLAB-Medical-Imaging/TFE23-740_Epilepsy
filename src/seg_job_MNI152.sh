12a94c35e3fc28094b838eca7386a45d6c77215b
Michele Cerra (michele.cerra@student.uclouvain.be) <mcerra@lm3-w001.cluster>
2023-04-18 20:45:17 +0200

    seg of MNI152

--- src/seg_job_MNI152.sh
+++ src/seg_job_MNI152.sh
@@ -16,9 +16,8 @@ module load freesurfer
 PROJECT_DIR=$HOME/Epilepsy-dMRI-VNS
 export SUBJECTS_DIR=$PROJECT_DIR/seg_subjs
 
-SUB_ID=subj01
+SUB_ID=MNI152
 
-srun --cpus-per-task=4 recon-all -all -s $SUB_ID -i $PROJECT_DIR/study/T1/subj00_T1.nii.gz -T2 $PROJECT_DIR/study/T1/subj00_T2.nii.gz -T2pial -qcache -expert $SUBJECTS_DIR/expert.opts
+srun --cpus-per-task=4 recon-all -all -s $SUB_ID -i $PROJECT_DIR/study/T1/MNI152_T1_05mm.nii.gz -qcache -expert $SUBJECTS_DIR/expert.opts
 
 srun --cpus-per-task=4 mri_cc -force -f -aseg aseg.mgz -o aseg.auto_CCseg.mgz $SUB_ID
-
