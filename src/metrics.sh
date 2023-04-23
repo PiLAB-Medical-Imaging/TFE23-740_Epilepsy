e093c10744bd915e82e12971b58879eae67f114b
micerr <fbmichele@hotmail.it>
2023-04-23 08:55:57 +0200

    fix metrics.sh

--- src/metrics.sh
+++ src/metrics.sh
@@ -3,19 +3,10 @@
 # Run metrics python script in backgroung and redirect the output in out.txt
 # metrics.py will create a sbatch job in the ceci cluster and wait the end.
 
-if [[ $# -ne 3 ]]; then
-	echo "Invalid number of parameters"
-	echo "[name ceci cluster] [path of the folder] [starting_state]"
-	exit 1
-fi
+mkdir $PROJECT_DIR/src/outputs
+rm $PROJECT_DIR/src/outputs/*
 
-if [[ ! -d $2 ]]; then
-	echo "Error: Directory $2 does not exists."
-	exit 1
-fi
-echo "Directory $2 exists."
-
-python3 metrics.py -w $1 -f $2 -s $3 > out.txt 2>&1 &
+python3 metrics.py -f $1 -m $2 > $PROJECT_DIR/src/outputs/out.txt 2>&1 &
 jobs
 
 exit 0
