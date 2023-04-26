78a8779b96245bf9bd73125b29643c7da84bdcb0
micerr <fbmichele@hotmail.it>
2023-04-26 13:45:35 +0200

    .

---
+++ src/sub_tractography.py
@@ -0,0 +1,71 @@
+import sys
+import os
+import elikopy
+import json
+
+from params import get_folder, get_segmentation
+from elikopy.utils import submit_job, get_job_state
+
+NTHREADS_FOREACH = 4
+
+def main():
+
+    ## Getting folder
+    folder_path = get_folder(sys.argv)
+    
+    # check if the user wants to compute the ODF and compute it
+    if "-odf" in sys.argv[1:]:
+        study = elikopy.core.Elikopy(folder_path, cuda=True, slurm=True, slurm_email="michele.cerra@student.uclouvain.be")
+
+        study.odf_msmtcsd()
+
+    time=[]; time[0]=0; time[1]=1 
+
+    extract_roi = False
+    if "-roi" in sys.argv[1:]:
+        extract_roi = True  
+        seg_path = get_segmentation(sys.argv)
+        time[1] += 15
+
+    reg = False
+    if "-reg" in sys.argv[1:]:
+        registration = True
+        time[0] += 1
+        time[1] += 15
+
+    tract = False
+    if "-tract" in sys.argv[1:]:
+        tract = True
+        time[0] += 1
+        time[1] += 15
+
+
+    ## Read the list of subjects and for each subject do the tractography
+    dest_success = folder_path + "/subjects/subj_list.json"
+    with open(dest_success, 'r') as file:
+        patient_list = json.load(file)
+
+    job_list = []
+
+    for p_code in patient_list:
+        p_job = {
+            "wrap" : "export MKL_NUM_THREADS=4 ; export OMP_NUM_THREADS=4 ; python -c 'from tractography import compute_tracts; compute_tracts(\"%s\", \"%s\", %s, \"%s\", %s, %s)'" % (p_code, folder_path, str(extract_roi), seg_path, str(reg), str(tract)),
+            "job_name" : "TRACKING_" + p_code,
+            "ntasks" : 1,
+            "cpu_per_task" : 4,
+            "mem_per_cpu" : 2048,
+            "time" : "%s:%s:00" % (str(time[0]), str(time[1])),
+            "mail_user" : "michele.cerra@student.uclouvain.be",
+            "mail_type" : "FAIL",
+            "output" : folder_path + '/subjects/' + p_code + "/dMRI/tractography/slurm-%j.out",
+            "error" : folder_path + '/subjects/' + p_code + "/dMRI/tractography/slurm-%j.err",
+        }
+        p_job_id = {}
+        p_job_id["id"] = submit_job(p_job)
+        p_job_id["name"] = p_code
+        job_list.append(p_job_id)
+    
+    get_job_state(folder_path, job_list, )
+
+if __name__ == "__main__":
+    exit(main())
