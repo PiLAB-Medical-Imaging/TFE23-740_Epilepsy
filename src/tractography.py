77152c92f49accebc577a514c7251eac15a867ad
micerr <fbmichele@hotmail.it>
2023-05-07 12:43:05 +0200

    anterior cingulate

--- src/tractography.py
+++ src/tractography.py
@@ -47,7 +47,8 @@ tracts = {
             {
                 "seed_images": ["Thalamus-Proper"],
                 "include_ordered" : ["plane-cingulum", "plane-cingulate", "frontal-cingulate"],
-                "angle" : 25
+                "angle" : 25,
+                "cutoff" : 0.07,
             },
         # "thalamus-Insula":
         #     {
