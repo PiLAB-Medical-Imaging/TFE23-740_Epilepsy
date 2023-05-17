20ca62efef06d84e1b763a42cecd5454391196ce
micerr <fbmichele@hotmail.it>
2023-05-17 11:36:52 +0200

    clean files

---
+++ src/clean.sh
@@ -0,0 +1,6 @@
+# Clean masks
+rm ../study/subjects/VNSLC_*/masks/*
+# Clean tracts
+rm ../study/subjects/VNSLC_*/dMRI/tractography/*
+# Clean registration
+rm ../study/subjects/VNSLC_*/registration/* ../study/subjects/VNSLC_*/registration/ants/*
