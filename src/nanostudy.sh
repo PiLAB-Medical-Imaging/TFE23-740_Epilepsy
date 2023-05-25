310114d2a69a6f31787d9ac754973de8a1f3061e
micerr <fbmichele@hotmail.it>
2023-05-25 19:34:30 +0200

    .

--- src/nanostudy.sh
+++ src/nanostudy.sh
@@ -1,1 +1,1 @@
-tar cvfz nanostudy.tar.gz  ./study/subjects/VNSLC_*/registration/* ./study/subjects/VNSLC_*/masks/* ./study/subjects/VNSLC_*/dMRI/tractography/* 
+tar cvfz $CECITRSF/nanostudy.tar.gz  ./study/subjects/VNSLC_*/registration/* ./study/subjects/VNSLC_*/masks/* ./study/subjects/VNSLC_*/dMRI/tractography/* 
