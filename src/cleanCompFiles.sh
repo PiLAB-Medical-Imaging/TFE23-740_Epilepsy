4e748877f9dd08fccfe9e918743b4b4301d3de87
micerr <fbmichele@hotmail.it>
2023-06-04 11:54:32 +0200

    .

---
+++ src/cleanCompFiles.sh
@@ -0,0 +1,3 @@
+rm -r study/subjects/VNSLC_*/dMRI/preproc/eddy/ study/subjects/VNSLC_*/dMRI/preproc/topup/ study/subjects/VNSLC*/dMRI/preproc/mppca/ study/subjects/VNSLC_*/dMRI/preproc/biasfield/
+
+rm -r study/subjects/sub*_E1/dMRI/preproc/eddy/ study/subjects/sub*_E1/dMRI/preproc/topup/ study/subjects/sub*_E1/dMRI/preproc/mppca/ study/subjects/sub*_E1/dMRI/preproc/biasfield/
