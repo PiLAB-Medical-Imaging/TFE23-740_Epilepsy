2a1c40f0cfde991d6f9a35f9c55cbb58f4f737ad
micerr <fbmichele@hotmail.it>
2023-06-26 19:33:05 +0200

    mod preproc

--- src/preproc.py
+++ src/preproc.py
@@ -7,10 +7,10 @@ from elikopy.utils import submit_job
 
 from params import *
 
-def preprocess(folder_path):
+def preprocess(folder_path, slurm=True):
 
     ## Connect
-    study : Elikopy = elikopy.core.Elikopy(folder_path, cuda=False, slurm=True, slurm_email="michele.cerra@student.uclouvain.be")
+    study : Elikopy = elikopy.core.Elikopy(folder_path, cuda=False, slurm=slurm, slurm_email="michele.cerra@student.uclouvain.be")
     study.patient_list()
 
     ## Preprocessing
@@ -31,40 +31,27 @@ def preprocess(folder_path):
 
         ## EDDY and MOTION correction
         eddy=True,
+        dynamic_susceptibility=True,
 
         ## BIAS FIELD correction
         biasfield=True,
 
         ## Registration
         qc_reg=True,
-
-        # CPUs
-        cpus=4
     )
+
+    study.white_mask(
+        maskType="wm_mask_FSL_T1",
+    )
+
     return 0
 
 def main():
 
     ## Getting Folder
     folder_path = get_folder(sys.argv)
-
-    job = {
-            "wrap" : "export MKL_NUM_THREADS=4 ; export OMP_NUM_THREADS=4 ; python -c 'from preproc import preprocess; preprocess(\"%s\")'" % (folder_path),
-            "job_name" : "PREPROC",
-            "ntasks" : 1,
-            "cpus_per_task" : 4,
-            "mem_per_cpu" : 1024,
-            "time" : "24:00:00",
-            "mail_user" : "michele.cerra@student.uclouvain.be",
-            "mail_type" : "FAIL",
-            "output" : folder_path + "/subjects/slurm-%j.out",
-            "error" : folder_path + "/subjects/slurm-%j.err",
-        }
-    job_id = {}
-    job_id["id"] = submit_job(job)
-    job_id["name"] = "master"
     
-    elikopy.utils.getJobsState(folder_path, [job_id], "log")
+    preprocess(folder_path, slurm=False)
 
 
 if __name__ == "__main__":
