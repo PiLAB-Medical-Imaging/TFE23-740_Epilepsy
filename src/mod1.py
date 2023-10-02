14912825a4f6277462f6ae632a60ac2011586fbe
micerr <fbmichele@hotmail.it>
2023-10-02 13:03:04 +0200

    new selection with sfs

--- src/mod1.py
+++ src/mod1.py
@@ -15,6 +15,7 @@ from ITMO_FS.filters import multivariate, univariate
 from sklearn.linear_model import LogisticRegressionCV
 from scipy import stats
 from feature_engine.selection import SmartCorrelatedSelection
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
 
 def cvMultivariateFilter(X, y, filter_name, algorithm=None, decision=False):
     if algorithm is None:
@@ -76,15 +77,15 @@ listOfAlgorithms = [
         },
         scoring="neg_log_loss", cv=3, n_jobs=1
     )),
-    # ("mlp", RandomizedSearchCV(
-    #     MLPClassifier(random_state=7, learning_rate="adaptive", max_iter=1000),
-    #     param_distributions={
-    #         "alpha": stats.loguniform(1e-3, 1e1),
-    #         "hidden_layer_sizes": [(100,), (50,), (20,)]
-    #     },
-    #     scoring="neg_log_loss", cv=3, n_jobs=1, random_state=7
-    # )),
-    # ("gauss", GaussianNB())
+    ("mlp", RandomizedSearchCV(
+        MLPClassifier(random_state=7, learning_rate="adaptive", max_iter=1000),
+        param_distributions={
+            "alpha": stats.loguniform(1e-3, 1e1),
+            "hidden_layer_sizes": [(100,), (50,), (20,)]
+        },
+        scoring="neg_log_loss", cv=3, n_jobs=1, random_state=7
+    )),
+    ("gauss", GaussianNB())
 ]
 
 def runMod3(X, y):
@@ -255,13 +256,150 @@ def runMod6(X, y):
 
         with open(f"../study/stats/results-{name}-loo-filter-Uni-Multi.json", "w") as outfile:
                 json.dump(cvs, outfile, indent=2, sort_keys=True)
+
+def runMod7(X, y):
+    print("Mod7")
+
+    for name, algorithm in listOfAlgorithms:
+        print(name)
+        decision = name == "svm"
+        cvs = {}
+        for filter_uni in [
+            univariate.f_ratio_measure,
+            univariate.spearman_corr,
+            univariate.kendall_corr,
+            univariate.reliefF_measure,
+        ]:
+            
+            for filter_multi_name in ["MRMR","CIFE","ICAP","DCSF","CFR","MRI"]:
+                try:
+                    union_names = filter_uni.__name__ + " " + filter_multi_name
+                    print(union_names)
+                    cv = cross_validate(
+                        Pipeline([
+                            ("selection", Pipeline([
+                                ("outlier", utils.MADOutlierRemotion(3)),
+                                ("scaler", RobustScaler()),
+                                ("univariate", univariate.UnivariateFilter(filter_uni, univariate.select_k_best(1000))),
+                                ("multivariate", multivariate.MultivariateFilter(filter_multi_name, 20)),
+                                ("sfs", SFS(
+                                    algorithm,
+                                    k_features=(1,15),
+                                    floating=True,
+                                    scoring="roc_auc",
+                                    cv=3,
+                                ))
+                            ])),
+                            ("clf", algorithm)
+                        ]),
+                        X, y,
+                        scoring=make_scorer(utils.retScores, needs_proba=not decision, needs_threshold=decision),
+                        cv=LeaveOneOut(),
+                        n_jobs=-1,
+                        verbose=2,
+                        error_score="raise",
+                    )
+                    cvs[union_names] = utils.printScores(y, cv["test_score"], decision=decision)
+                except:
+                    print("Error: ", union_names)
+                    cvs[union_names] = "Error"
+
+        with open(f"../study/stats/results-{name}-loo-filter-Uni-Multi-sfs.json", "w") as outfile:
+                json.dump(cvs, outfile, indent=2, sort_keys=True)
+
+def runMod7_1(X, y):
+    print("Mod7_1")
+
+    for name, algorithm in listOfAlgorithms:
+        print(name)
+        decision = name == "svm"
+        cvs = {}
+        for filter_name in ["MRMR","CIFE","ICAP","DCSF","CFR","MRI"]:
+            try:
+                print(filter_name)
+                cv = cross_validate(
+                    Pipeline([
+                        ("selection", Pipeline([
+                            ("outlier", utils.MADOutlierRemotion(3)),
+                            ("scaler", RobustScaler()),
+                            ("t-test", utils.MannwhitenFilter(0.05)),
+                            ("correlated", SmartCorrelatedSelection(threshold=0.95,missing_values="raise", selection_method="variance")),
+                            ("multivariate", multivariate.MultivariateFilter(filter_name, 20)),
+                            ("sfs", SFS(
+                                algorithm,
+                                k_features=(1,15),
+                                floating=True,
+                                scoring="roc_auc",
+                                cv=3,
+                            ))
+                        ])),
+                        ("clf", algorithm)
+                    ]),
+                    X, y,
+                    scoring=make_scorer(utils.retScores, needs_proba=not decision, needs_threshold=decision),
+                    cv=LeaveOneOut(),
+                    n_jobs=-1,
+                    verbose=2,
+                    error_score="raise",
+                )
+                cvs[filter_name] = utils.printScores(y, cv["test_score"], decision=decision)
+            except:
+                print("Error: ", filter_name)
+                cvs[filter_name] = "Error"
+
+        with open(f"../study/stats/results-{name}-loo-filter-Man-Multi-sfs.json", "w") as outfile:
+                json.dump(cvs, outfile, indent=2, sort_keys=True)
+
+def runMod7_2(X, y, y3):
+    print("Mod7_2")
+
+    for name, algorithm in listOfAlgorithms:
+        print(name)
+        decision = name == "svm"
+        cvs = {}
+        for filter_name in ["MRMR","CIFE","ICAP","DCSF","CFR","MRI"]:
+            try:
+                print(filter_name)
+                cv = cross_validate(
+                    Pipeline([
+                        ("selection", Pipeline([
+                            ("outlier", utils.MADOutlierRemotion(3)),
+                            ("scaler", RobustScaler()),
+                            ("t-test", utils.MannwhitenFilter(0.05)),
+                            ("ANOVA", utils.KruskalFilter(y3, 0.05))
+                            ("correlated", SmartCorrelatedSelection(threshold=0.95,missing_values="raise", selection_method="variance")),
+                            ("multivariate", multivariate.MultivariateFilter(filter_name, 20)),
+                            ("sfs", SFS(
+                                algorithm,
+                                k_features=(1,15),
+                                floating=True,
+                                scoring="roc_auc",
+                                cv=3,
+                            ))
+                        ])),
+                        ("clf", algorithm)
+                    ]),
+                    X, y,
+                    scoring=make_scorer(utils.retScores, needs_proba=not decision, needs_threshold=decision),
+                    cv=LeaveOneOut(),
+                    n_jobs=-1,
+                    verbose=2,
+                    error_score="raise",
+                )
+                cvs[filter_name] = utils.printScores(y, cv["test_score"], decision=decision)
+            except:
+                print("Error: ", filter_name)
+                cvs[filter_name] = "Error"
+
+        with open(f"../study/stats/results-{name}-loo-filter-Man-Multi-sfs.json", "w") as outfile:
+                json.dump(cvs, outfile, indent=2, sort_keys=True)
             
 def main():
     
     df = utils.getReducedDS()
     X, y, y3 = utils.splitFeatureLabels(df)
 
-    runMod5_1(X, y)
+    runMod7(X, y)
 
 if __name__ == "__main__":
     exit(main())
