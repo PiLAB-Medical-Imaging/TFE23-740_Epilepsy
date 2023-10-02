962629876883be4210aa06029347b69d424d13aa
micerr <fbmichele@hotmail.it>
2023-10-02 21:59:37 +0200

    .

--- src/mod1.py
+++ src/mod1.py
@@ -295,7 +295,7 @@ def runMod7(X, y):
                         X, y,
                         scoring=make_scorer(utils.retScores, needs_proba=not decision, needs_threshold=decision),
                         cv=LeaveOneOut(),
-                        n_jobs=3,
+                        n_jobs=6,
                         verbose=2,
                         error_score="raise",
                     )
@@ -399,7 +399,7 @@ def main():
     df = utils.getReducedDS()
     X, y, y3 = utils.splitFeatureLabels(df)
 
-    runMod7_2(X, y, y3)
+    runMod7(X, y)
 
 if __name__ == "__main__":
     exit(main())
