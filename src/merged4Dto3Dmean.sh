085934c495b71a6d946d09e9ecd20c64664b41d3
micerr <fbmichele@hotmail.it>
2023-10-06 00:00:30 +0200

    .

---
+++ src/merged4Dto3Dmean.sh
@@ -0,0 +1,5 @@
+for i in $(ls -la ../study/freesurfer/VNSLC_*/dpath/merged_avg16_syn_bbr.mgz | cut -d " " -f 10); do
+    echo $i
+    new_name="$(dirname $i)/mergedX2_3D_avg16_syn_bbr.nii.gz"
+    mrmath $i mean $new_name -axis 3
+done
